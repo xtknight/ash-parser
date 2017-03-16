0900b10381e350c8f813a17cabec5ed19b853643
Andrew Matteson <xt.knight@gmail.com>
2017-03-17 02:37:53 +0900

    fix training test

--- training_test.sh
+++ training_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm -f /tmp/testmodelEN/model.ckpt.* /tmp/testmodelEN/checkpoint
+# the sample provided training corpus contains 931 feature bags with this configuration
 mkdir -p /tmp/testmodelEN
-cp parser-config.sample /tmp/testmodelEN/
-python3 dep_parser.py /tmp/testmodelEN corpus/en-ud-train.conllu  corpus/en-ud-test.conllu  --train
+cp parser-config.sample /tmp/testmodelEN/parser-config
+python3 dep_parser.py /tmp/testmodelEN corpus/en-ud-train.conllu  corpus/en-ud-test.conllu  --train --epochs 5 --restart
